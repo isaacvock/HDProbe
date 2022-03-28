534a42005bfab45fa3614d359572d5f2e17074b6
Isaac <isaac.vock@yale.edu>
2022-03-27 20:38:17 -0400

    Added simuation and maketidy functions

---
+++ R/Simulate.R
@@ -0,0 +1,86 @@
+#' Simulate heteroskedastic mutational probing data
+#'
+#' @param npos Numeric; number of positions to simulate mutational data for
+#' @param nreps Numeric; number of replicates to simulate for each of 2 conditions
+#' @param het_m Numeric; slope of sequencing depth-variance relationship on log-log scale
+#' @param het_b Numeric; intercept of sequencing depth-variance relationship on log-log scale
+#' @param nsig Numeric; number of significant changes in mutation rate
+#' @param mu_e Numeric; differences in logit(mutation rate) for significant nts drawn from normal(mu_e, sig_e)
+#' @param sig_e Numeric; effect size parameter (see mu_e for details)
+#' @param lp_mean Numeric; average logit(mutation rate)
+#' @param lp_sd Numeric; standard deviation of logit(mutation rates)
+#' @param r_mean Numeric; Avg. read counts drawn from lognormal(r_mean, r_sd)
+#' @param r_sd Numeric; read counts drawn from lognormal(r_mean, r_sd)
+#' @param sig_T Numeric; standard deviation of logit(p) variances on log scale
+#' @param phi Numeric; read counts for each sample drawn from negative binomial distribution with size = phi
+#'
+#' @return List with 2 componenets, one of which is a dataframe that can be passed to HDProbe
+#' @export
+sim_muts <- function(npos, nreps = 3,
+                     het_m = -0.7, het_b = 1.3,
+                     nsig = round(npos/10), mu_e = 0, sig_e = 0.75,
+                     lp_mean = -4.35, lp_sd = 0.8,
+                     r_mean = 6.75, r_sd = 1.2, sig_T = 0.05,
+                     phi = 20){
+
+  ### Simulate read coverage mean
+  cov_means <- stats::rlnorm(npos*2, meanlog = r_mean, sdlog = r_sd)
+
+  ### Simulate reads in each replicate
+
+  reads <- stats::rnbinom(npos*nreps*2, mu = rep(cov_means, each = nreps), size = phi)
+
+
+  ### Simulate heteroskedastic mutation rates
+
+  ## Simulate mutation rate effect sizes
+  effs <- c(rep(0, times = npos), rep(0, times = npos - nsig), stats::rnorm(nsig, mean = mu_e, sd = sig_e))
+
+  ## Avg. ctl rate for each position
+  avg_lps <- stats::rnorm(npos, mean = lp_mean, sd = lp_sd)
+
+  avg_lps <- rep(avg_lps, times = 2) + effs
+
+  var_sds <- sqrt(10^(stats::rnorm(npos*nreps*2, mean = het_m*log10(rep(cov_means, each = nreps)), sd = sig_T)) )
+
+  ## Individual rate for each position and each replicate
+  lps <- stats::rnorm(npos*nreps*2, mean = rep(avg_lps, each = nreps), sd = var_sds)
+
+
+
+  ### Simulate mutational data
+
+  nmuts <- stats::rbinom(npos*nreps*2, size = reads, prob = pracma::sigmoid(lps))
+
+
+  ### Create tibble
+
+  mut_df <- tibble::tibble(cov = reads, muts = nmuts,
+                           rep_id = rep(1:nreps, times = npos*2),
+                           pos_id = rep(rep(1:npos, each = nreps), times = 2),
+                           exp_id = rep(1:2, each = npos*nreps))
+
+
+
+  ##### Analyze with HDProbe
+  colnames(mut_df) <- c("ntrials", "nmuts", "R_ID",
+                        "P_ID", "E_ID")
+
+  avg_truth <- tibble::tibble(logit_p = avg_lps,
+                              logit_effs = effs,
+                              P_ID = rep(1:npos, times = 2),
+                              E_ID = rep(1:2, each = npos))
+
+  rep_truth <- tibble::tibble(logit_p = lps,
+                              P_ID = mut_df$P_ID,
+                              E_ID = mut_df$E_ID,
+                              R_ID = mut_df$R_ID)
+
+
+  sim_list <- list(avg_truth = avg_truth,
+                   rep_truth = rep_truth,
+                   mut_df = mut_df)
+
+  return(sim_list)
+
+}
---
+++ R/maketidy.R
@@ -0,0 +1,165 @@
+#' Put mutation probing data into tidy format
+#'
+#' @param wt_muts Dataframe from csv containing mutation profiling data for reference sample
+#' @param ko_muts Dataframe from csv containing mutation profiling data for experimental sample
+#' @param nreps Numeric; number of replicates
+#' @param target Logical; if TRUE, then data is treated as targeted probing data
+#' @param SHAPE Logical; if TRUE, then data is treated as SHAPE data rather than DMS-seq data
+#' @param totcut Numeric; read count cutoff to be considered for analysis
+#' @param rate_cut Numeric; 1/(max mutation rate) allowed; used for SNP filtering
+#'
+#' @return a dataframe that can be passed to HDProbe
+#' @export
+maketidy <- function(wt_muts, ko_muts, nreps, target = FALSE, SHAPE = FALSE,
+                     totcut = 500, rate_cut = 4){
+  ## Filter out anything with less than totcut reads and anomalously high mutation rates
+  # Also filtering for any mutations for investigation purposes
+
+  if(nreps == 3){
+    wt_muts <- wt_muts %>% dplyr::filter(R1_cov > totcut  & R2_cov > totcut & R3_cov > totcut) %>%
+      dplyr::filter(R1_mut < R1_cov/rate_cut & R2_mut < R2_cov/rate_cut & R3_mut < R3_cov/rate_cut)
+
+    ko_muts <- ko_muts %>% dplyr::filter(R1_cov > totcut  & R2_cov > totcut & R3_cov > totcut) %>%
+      dplyr::filter(R1_mut < R1_cov/rate_cut & R2_mut < R2_cov/rate_cut & R3_mut < R3_cov/rate_cut)
+
+  }else if(nreps == 2){
+    wt_muts <- wt_muts %>% dplyr::filter(R1_cov > totcut  & R2_cov > totcut) %>%
+      dplyr::filter(R1_mut < R1_cov/rate_cut & R2_mut < R2_cov/rate_cut)
+
+    ko_muts <- ko_muts %>% dplyr::filter(R1_cov > totcut  & R2_cov > totcut) %>%
+      dplyr::filter(R1_mut < R1_cov/rate_cut & R2_mut < R2_cov/rate_cut)
+
+  }else{
+    stop("Isaac needs to write better code")
+  }
+
+
+
+  if(target){
+    wt_nts <- wt_muts[,c("pos")]
+
+  }else{
+    wt_nts <- wt_muts[,c("chr", "pos", "strand")]
+
+  }
+
+  ### Now for KO data
+
+
+
+
+  if(target){
+    ko_nts <- ko_muts[,c("pos")]
+    # Find nts in both ko and wt
+
+    both_nts <- dplyr::semi_join(ko_nts, wt_nts)
+
+
+    # Filter out for only nts in both datasets
+    wt_muts <- dplyr::semi_join(wt_muts, both_nts, by = c("pos") )
+    ko_muts <- dplyr::semi_join(ko_muts, both_nts, by = c("pos"))
+
+
+    if(!SHAPE){
+      # Filter out for mutations that can occur in DMS-seq
+      wt_muts <- wt_muts %>% dplyr::filter(nt %in% c("A", "C"))
+      ko_muts <- ko_muts %>% dplyr::filter(nt %in% c("A", "C"))
+    }
+
+
+
+    ### Convert to tidy format and combine data
+
+    muts <- rep(0, times = nrow(wt_muts)*2*nreps)
+    covs <- muts
+    R_ID <- muts
+    E_ID <- muts
+    strand <- muts
+
+    nsites <- nrow(wt_muts)
+
+
+    for(i in 1:nreps){
+
+      mut_id <- grep(paste0("R", i, "_mut"), colnames(wt_muts))
+      cov_id <- grep(paste0("R", i, "_cov"), colnames(wt_muts))
+
+      muts[(2*(i-1)*nsites + 1):(2*i*nsites)]  <- c(unclass(wt_muts[,mut_id])[[1]], unclass(ko_muts[,mut_id])[[1]])
+      covs[(2*(i-1)*nsites + 1):(2*i*nsites)]  <- c(unclass(wt_muts[,cov_id])[[1]], unclass(ko_muts[,cov_id])[[1]])
+
+      R_ID[(2*(i-1)*nsites + 1):(2*i*nsites)] <- rep(i, times = nrow(wt_muts)*2)
+      E_ID[(2*(i-1)*nsites + 1):(2*i*nsites)] <- rep(1:2, each = nrow(wt_muts))
+
+
+    }
+
+    Muts_df <- tibble::tibble(P_ID = rep(1:nrow(wt_muts), times = nreps*2),
+                              nmuts = muts,
+                              ntrials = covs,
+                              R_ID = R_ID,
+                              E_ID = E_ID,
+                              pos = rep(wt_muts$pos, times = nreps*2))
+
+    return(Muts_df)
+  }else{
+    ko_nts <- ko_muts[,c("chr", "pos", "strand")]
+
+    # Find nts in both ko and wt
+
+    both_nts <- dplyr::semi_join(ko_nts, wt_nts)
+
+
+    # Filter out for only nts in both datasets
+    wt_muts <- dplyr::semi_join(wt_muts, both_nts, by = c("chr", "pos", "strand"))
+    ko_muts <- dplyr::semi_join(ko_muts, both_nts, by = c("chr", "pos", "strand"))
+
+    if(!SHAPE){
+      # Filter out for mutations that can occur in DMS-seq
+      wt_muts <- wt_muts %>% dplyr::filter(nt %in% c("A", "C"))
+      ko_muts <- ko_muts %>% dplyr::filter(nt %in% c("A", "C"))
+    }
+
+
+
+    ### Convert to tidy format and combine data
+
+    muts <- rep(0, times = nrow(wt_muts)*2*nreps)
+    covs <- muts
+    R_ID <- muts
+    E_ID <- muts
+    strand <- muts
+
+    nsites <- nrow(wt_muts)
+
+
+    for(i in 1:nreps){
+
+      mut_id <- grep(paste0("R", i, "_mut"), colnames(wt_muts))
+      cov_id <- grep(paste0("R", i, "_cov"), colnames(wt_muts))
+
+      muts[(2*(i-1)*nsites + 1):(2*i*nsites)]  <- c(unclass(wt_muts[,mut_id])[[1]], unclass(ko_muts[,mut_id])[[1]])
+      covs[(2*(i-1)*nsites + 1):(2*i*nsites)]  <- c(unclass(wt_muts[,cov_id])[[1]], unclass(ko_muts[,cov_id])[[1]])
+
+      R_ID[(2*(i-1)*nsites + 1):(2*i*nsites)] <- rep(i, times = nrow(wt_muts)*2)
+      E_ID[(2*(i-1)*nsites + 1):(2*i*nsites)] <- rep(1:2, each = nrow(wt_muts))
+
+
+    }
+
+    Muts_df <- tibble::tibble(P_ID = rep(1:nrow(wt_muts), times = nreps*2),
+                              nmuts = muts,
+                              ntrials = covs,
+                              R_ID = R_ID,
+                              E_ID = E_ID,
+                              chr = rep(wt_muts$chr, times = nreps*2),
+                              pos = rep(wt_muts$pos, times = nreps*2),
+                              strand = rep(wt_muts$strand, times = nreps*2))
+
+    return(Muts_df)
+
+  }
+
+
+
+
+}
